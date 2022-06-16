51d5fbe33d25b07ca91218d437333e85b7373f07
jwcastillo <jwcastillo@gmail.com>
2022-06-16 12:13:49 -0400

    chore: add login appcenter

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,15 +19,15 @@ fi
 if [ -n "${INPUT_BUILDNUMBER}" ]; then
     params+=(--build-number "$INPUT_BUILDNUMBER")
 fi
-
+appcenter login --token $INPUT_TOKEN
 for group in $INPUT_GROUP; do
     if ${isFirst} ; then
         isFirst=false
-        appcenter login --token "$INPUT_TOKEN" 
         appcenter distribute release --app "$INPUT_APPNAME" --group $group --file "$INPUT_FILE" --release-notes "$RELEASE_NOTES" "${params[@]}"
         releaseId=$(appcenter distribute releases list  --app "$INPUT_APPNAME" | grep ID | tr -s ' ' | cut -f2 -d ' ' | sort -n -r | head -1)
     else
-        appcenter login --token "$INPUT_TOKEN" 
-        appcenter distribute releases add-destination --token "$INPUT_TOKEN" -d $group -t group -r $releaseId --app "$INPUT_APPNAME" "${params[@]}"
+        appcenter distribute releases add-destination -d $group -t group -r $releaseId --app "$INPUT_APPNAME" "${params[@]}"
+
     fi
 done
+appcenter logout
